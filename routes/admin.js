ea8406b8d01ca2eede43f481e67e1d97846ce892
Jimmy Zhang <Jzjimmy3@gmail.com>
2021-09-18 18:50:38 -0400

    Restyling and incorporating pub

--- routes/admin.js
+++ routes/admin.js
@@ -1,22 +1,22 @@
-const express = require("express");
+const express = require('express');
 
-const path = require("path");
+const path = require('path');
 
-const rootDir = require("../util/path");
+const rootDir = require('../util/path');
 
 const router = express.Router();
 
 const products = [];
 
-router.get("/add-product", (req, res, next) => {
-  console.log("In another middleware");
-  res.sendFile(path.join(rootDir, "views", "add-product.html"));
+router.get('/add-product', (req, res, next) => {
+  console.log('In another middleware');
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
-router.post("/add-product", (req, res, next) => {
+router.post('/add-product', (req, res, next) => {
   products.push({ title: req.body.title });
   console.log(req.body);
-  res.redirect("/");
+  res.redirect('/');
 });
 
 exports.routes = router;
--- routes/shop.js
+++ routes/shop.js
@@ -1,12 +1,14 @@
-const express = require("express");
-const path = require("path");
-const rootDir = require("../util/path");
-const adminData = require("./admin");
+const path = require('path');
+const express = require('express');
+const rootDir = require('../util/path');
+const adminData = require('./admin');
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  console.log(adminData.products);
-  res.sendFile(path.join(rootDir, "views", "shop.html"));
+router.get('/', (req, res, next) => {
+  const products = adminData.products;
+  res.render('shop', { prods: products, docTitle: 'Shop' });
+  // console.log(adminData.products);
+  // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
 module.exports = router;
