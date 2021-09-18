1dd3e51298c767242d0f055e485054a618d9f767
Jimmy Zhang <Jzjimmy3@gmail.com>
2021-09-18 17:25:57 -0400

    Installation of Pug, EJS, and Handlebars

--- routes/admin.js
+++ routes/admin.js
@@ -6,14 +6,18 @@ const rootDir = require("../util/path");
 
 const router = express.Router();
 
+const products = [];
+
 router.get("/add-product", (req, res, next) => {
   console.log("In another middleware");
   res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 
 router.post("/add-product", (req, res, next) => {
+  products.push({ title: req.body.title });
   console.log(req.body);
   res.redirect("/");
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
